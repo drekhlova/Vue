f795afacdb7345dbdf9072ea84f8f11515335d22
Daria Rekhlova <dolly1993@yandex.ru>
2024-05-04 16:48:18 +0400

    Added HW2

--- Homework/Scripts/script.js
+++ Homework/Scripts/script.js
@@ -11,7 +11,7 @@ new Vue({
       {
         image: "blog_photo2.jpg",
         imgText: "Living Design",
-        itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+        itemTitle: "Low Cost Latest Invented Interior Designing Ideas",
         itemData: "22 December,2022",
       },
       {
@@ -21,37 +21,19 @@ new Vue({
         itemData: "25 December,2022",
       },
       {
-        image: "blog_photo1.jpg",
+        image: "blog_photo4.jpg",
         imgText: "Kitchan Design",
         itemTitle: "Let’s Get Solution For Building Construction Work",
         itemData: "26 December,2022",
       },
       {
-        image: "blog_photo2.jpg",
+        image: "blog_photo5.jpg",
         imgText: "Living Design",
-        itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+        itemTitle: "Low Cost Latest Invented Interior Designing Ideas",
         itemData: "22 December,2022",
       },
       {
-        image: "blog_photo3.jpg",
-        imgText: "Interior Design",
-        itemTitle: "Best For Any Office & Business Interior Solution",
-        itemData: "25 December,2022",
-      },
-      {
-        image: "blog_photo1.jpg",
-        imgText: "Kitchan Design",
-        itemTitle: "Let’s Get Solution For Building Construction Work",
-        itemData: "26 December,2022",
-      },
-      {
-        image: "blog_photo2.jpg",
-        imgText: "Living Design",
-        itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
-        itemData: "22 December,2022",
-      },
-      {
-        image: "blog_photo3.jpg",
+        image: "blog_photo6.jpg",
         imgText: "Interior Design",
         itemTitle: "Best For Any Office & Business Interior Solution",
         itemData: "25 December,2022",
