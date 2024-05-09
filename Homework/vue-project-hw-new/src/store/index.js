c14e1b5fc6f9d4cf47e79e79c720f60b36def25e
Daria Rekhlova <dolly1993@yandex.ru>
2024-05-09 20:03:13 +0400

    added HW7

--- Homework/vue-project-hw-new/src/store/index.js
+++ Homework/vue-project-hw-new/src/store/index.js
@@ -5,13 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    slides: []
   },
   getters: {
+    slides (state) {
+      return state.slides
+    }
   },
   mutations: {
+    SET_SLIDES (state, slides) {
+      state.slides = slides
+    }
   },
   actions: {
+    fetchSlides ({ commit }) {
+      commit('SET_SLIDES', [
+        {
+          alt: 'image 1',
+          url: 'project_details_photo.jpg'
+        },
+        {
+          alt: 'image 2',
+          url: 'project_details_photo.jpg'
+        },
+        {
+          alt: 'image 3',
+          url: 'project_details_photo.jpg'
+        }
+      ])
+    }
   },
-  modules: {
-  }
+
+  modules: {}
 })
